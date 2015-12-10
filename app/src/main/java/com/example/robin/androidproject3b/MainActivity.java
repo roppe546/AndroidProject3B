a4761853ed36a495eff914d72a31f28d661fbf65
Peonsson <xsnudelx@hotmail.com>
2015-12-10 10:56:24 +0100

    Implemented alpha GUI with a text for pulse and a button to stop and start downloading.

--- app/src/main/java/com/example/robin/androidproject3b/MainActivity.java
+++ app/src/main/java/com/example/robin/androidproject3b/MainActivity.java
@@ -2,14 +2,34 @@ package com.example.robin.androidproject3b;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(), "Hello World!", Toast.LENGTH_SHORT).show();
+
+        textView = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(button.getText().equals("Start"))
+                    button.setText("Stop");
+                else
+                    button.setText("Start");
+
+                //TODO implement download logic
+            }
+        });
     }
 }
