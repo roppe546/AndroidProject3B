8acf27ba17646bd23014294ba486e57718cfb8a7
roppe546 <roppe546@gmail.com>
2015-12-11 16:16:09 +0100

    Remove unused preferences from Settings Activity.

--- app/src/main/java/com/example/robin/androidproject3b/SettingsActivity.java
+++ app/src/main/java/com/example/robin/androidproject3b/SettingsActivity.java
@@ -1,8 +1,6 @@
 package com.example.robin.androidproject3b;
 
 
-import android.app.ActionBar;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
